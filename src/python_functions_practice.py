3fee4f4e9f10f0485ed07c194445b70ba073da18
Zoe Eustace <zoeeustace@Zoes-MacBook-Air.local>
2023-03-23 12:56:51 +0000

    modified something

--- src/python_functions_practice.py
+++ src/python_functions_practice.py
@@ -13,6 +13,7 @@ def multiply(number_1, number_2):
 
 def divide(number_1, number_2):
     return number_1 / number_2
+# use // for division if looking for integer, not float
 
 def length_of_string(test_string):
     return len(test_string)
@@ -22,6 +23,8 @@ def join_string(string_1, string_2):
 
 def add_string_as_number(string_1, string_2):
     return int(string_1) + int(string_2)
+    #return add(int(string_1), int(string_2))
+    # as already used the add function earlier on (Q2)
 
 months = {1: "January", 3: "March", 9: "September"
 }
@@ -32,25 +35,34 @@ def number_to_full_month_name(month_number):
 # def number_to_full_month_name(months_number):
 #     if months_number == 1:
 #         return "January"
-#     if months_number == 3:
+#     elif months_number == 3:
 #         return "March"
-#     if months_number == 9:
+#     elif months_number == 9:
 #         return "September"    
 
 
 def number_to_short_month_name(number_short_month):
     if number_short_month == 1:
         return "Jan"
-    if number_short_month == 4:
+    elif number_short_month == 4:
         return "Apr"
-    if number_short_month == 10:
+    elif number_short_month == 10:
         return "Oct"
+# def number_to_short_month_name(month_number):
+    # short_month_name = number_to_short_month_name[0:3]
+    # return short_month_name
+
 
 def volume_of_cube(l, w, h):
     return l * w * h
+# def volume_of_cube(length_of_side):
+    # return ** length_of_side
 
 def reverse_string(string):
     return string[:: -1]
+    # "".join(reversed(string))
 
 def fahrenheit_to_celsius(temp):
     return (temp - 32) * (5/9)
+    # celsius = (temp - 32) * (5.0/9.0)
+    # return round(celsius, 2)
